0dd7cfc849d97388c57c1757e18aca03b8ad1290
Amirali-soltan <amiralisoltan.official@gmail.com>
2024-10-14 01:38:49 +0330

    'host'

--- merch_market/settings.py
+++ merch_market/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^(lhcd*qb95l@utrk%d=h%$u=l*p^9=xk4$_l7)lnltnayc1mt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['amiralis0ltan.pythonanywhere.com']
 
 
 # Application definition
